6f65372e4b72d39d4d1a742b3955d2308df5f81f
Jors Academy <abalci16@posta.pau.edu.tr>
2026-09-19 23:59:34 +0300

    Add manufacturing optimization package

---
+++ src/manufacturing_optimization/__init__.py
@@ -0,0 +1,14 @@
+"""Manufacturing process optimization case studies."""
+
+__all__ = [
+    "case01_production_maintenance",
+    "case02_milkrun",
+    "case03_agv_assignment",
+    "case04_worker_rotation",
+    "case05_quality_mdp",
+    "case06_cnc_process",
+    "case07_tooling_scheduling",
+    "case08_conwip",
+    "case09_energy_scheduling",
+    "case10_cutting_stock",
+]
